09eaa1a7f7e2307ba0a05e47c5476656a3312eda
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2024-08-28 16:02:28 +0530

    Removed Redundant Import

--- src/components/UserDetail.jsx
+++ src/components/UserDetail.jsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 async function getUserDetail(setUserDetailResponse){
 
