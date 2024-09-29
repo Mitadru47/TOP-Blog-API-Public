09462f5d4ec80e3631403015a442dbebd039990e
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2024-09-29 19:46:59 +0530

    CommentCreator Minor Updates

--- src/components/CommentCreator.jsx
+++ src/components/CommentCreator.jsx
@@ -40,30 +40,8 @@ function handleSubmit(event, id){
 
         .then((response) => response.json())
         .then((responseBody) => {
-    
-            if(responseBody.status === "Failure!"){
-            
-                // Displaying error messages received from backend
-                
-                let message = document.getElementById("comment-failed-info");
 
-                message.classList.remove("display-off");
-                message.classList.add("display-on");
-
-                let err = "";
-                for(let i=0; i<responseBody.error.length; i++){
-                
-                    if(i === responseBody.error.length - 1)
-                        err = err + responseBody.error[i].msg;
-
-                    else
-                        err = err + responseBody.error[i].msg + "\n ";
-                }
-
-                message.innerText = err;
-            }
-
-            else{
+            if(responseBody.status === "Success!"){
 
                 let message = document.getElementById("comment-failed-info");
 
@@ -77,6 +55,29 @@ function handleSubmit(event, id){
                 username.value = plainFormData.username;
                 email.value = plainFormData.email;
             }
+
+            else{
+            
+                let message = document.getElementById("comment-failed-info");
+
+                message.classList.remove("display-off");
+                message.classList.add("display-on");
+
+                // Displaying error messages received from backend
+
+                let err = "";
+                
+                for(let i=0; i<responseBody.error.length; i++){
+                
+                    if(i === responseBody.error.length - 1)
+                        err = err + responseBody.error[i].msg;
+
+                    else
+                        err = err + responseBody.error[i].msg + "\n ";
+                }
+
+                message.innerText = err;
+            }
             
             // Re-enabling Inputs after POST
 
@@ -84,8 +85,8 @@ function handleSubmit(event, id){
             username.disabled = false;
             email.disabled = false;
             submit.disabled = false;
-        }
-    )
+        })
+        
         .catch((error) => console.log(error));  
 }
 
@@ -99,12 +100,12 @@ function CommentCreator({ post }){
 
                 <form onSubmit={(event) => handleSubmit(event, post[0]._id)}>
                     
-                    <textarea id="body-input" name="body" cols="150" rows="4" defaultValue="" placeholder="Comment" required></textarea>
+                    <textarea id="body-input" name="body" cols="150" rows="4" defaultValue="" placeholder="Comment"></textarea>
                     
                     <br></br>
                 
-                    <input id="username-input" type="text" name="username" defaultValue="" placeholder="Username" required></input>
-                    <input id="email-input" type="email" name="email" defaultValue="" placeholder="Email" required></input>
+                    <input id="username-input" type="text" name="username" defaultValue="" placeholder="Username"></input>
+                    <input id="email-input" type="email" name="email" defaultValue="" placeholder="Email"></input>
                 
                     <input id="post-input" type="text" name="post"  value={post[0]._id} readOnly></input>
                     <button id="submit-button" type="submit">Comment</button>
